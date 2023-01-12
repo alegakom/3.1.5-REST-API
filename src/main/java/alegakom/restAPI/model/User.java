6f808e8b9c44a50710c48e6c1cb386f9242a4a08
alegakom <alegakom@gmail.com>
2023-01-12 21:42:50 +0300

    last version
    не работает на view, но контроллеры работают, через postman проверено. Буду со временем докручивать, доведу до идеала, что бы было

--- src/main/java/alegakom/restAPI/model/User.java
+++ src/main/java/alegakom/restAPI/model/User.java
@@ -31,7 +31,7 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "users_id"),
